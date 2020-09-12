65530c0d7405b67ce0c1655ae83292d5e59e609f
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 18:46:15 +0300

    Updates

---
+++ precache-manifest.0ace88510c0e0c0de17c3d1e090a4ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa1fbc9b09ae5b7c6eb97dcaf93af55",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "aed4319aaee6a8c81ce0",
+    "url": "/random-quote-machine/static/css/main.c921405e.chunk.css"
+  },
+  {
+    "revision": "f614ac511b0ffca3b46e",
+    "url": "/random-quote-machine/static/js/2.5e808a5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.5e808a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed4319aaee6a8c81ce0",
+    "url": "/random-quote-machine/static/js/main.b631664f.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
